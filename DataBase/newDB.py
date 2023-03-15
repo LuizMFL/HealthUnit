f2095ff17da5206d33be946543c897af969fb790
VoidPHF <106350565+VoidPHF@users.noreply.github.com>
2023-03-15 13:15:55 -0300

    [feat] Add context manager for connection database

--- DataBase/newDB.py
+++ DataBase/newDB.py
@@ -1,18 +1,50 @@
-import mysql.connector
+import pymysql.cursors
+from contextlib import contextmanager
 from pathlib import Path
 
 
 class DataBase:
     def __init__(self) -> None:
-        self.PATH_DataSets = str(Path(__file__).parent / "Datasets")
-        self.mydb = mydb = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="Lu1zM1gu3l#My$QL"
-        )
-        self.mydb.cursor
-        print(self.mydb)
+        self.PATH_DataSets = str(Path(__file__).parent / 'Datasets')
+        self.config = {
+            'user': 'root',
+            'password': 'Lu1zM1gu3l#My$QL',
+            'host': 'localhost',
+            'cursorclass': pymysql.cursors.DictCursor,
+            'charset': 'utf8mb4'
+        }
 
+    def create_DB(self):
+        with self.connect_LocalHost() as conn:
+            with conn.cursor() as cursor:
+                if 
+    @contextmanager
+    def connect_LocalHost(self):
+        conn = pymysql.connect(
+            host=self.config['host'],
+            user=self.config['user'],
+            password=self.config['password'],
+            charset=self.config['charset'],
+            cursorclass=self.config['cursorclass'])
+        try:
+            yield conn
+        finally:
+            conn.close()
+
+    @contextmanager
+    def connect_DB(self, DB_name:str):
+        conn = pymysql.connect(
+            host=self.config['host'],
+            user=self.config['user'],
+            password=self.config['password'],
+            db=DB_name,
+            charset=self.config['charset'],
+            cursorclass=self.config['cursorclass']  # Quando fizermos uma consulta, teremos um DICT
+        )
+        try:
+            yield conn  # Vai retornar a conexão e continuar quando for chamda de novo
+        finally:
+            conn.close()
 
 if __name__ == "__main__":
     db = DataBase()
