c71496b6ce3dcc3a077574b18a3bd393f5008746
VoidPHF <106350565+VoidPHF@users.noreply.github.com>
2023-04-03 15:54:50 -0300

    [Refactor] Modified Get and Insert in Paciente to correct response

--- Paciente/PacienteFunctions.py
+++ Paciente/PacienteFunctions.py
@@ -11,28 +11,37 @@ class Paciente:
         response = {'Response': (406, 'Failed')}
         if len(cpf) == 11:
             response_pe = self.get_pessoa(cpf)
-            response = dict(response_pe)
-            response.pop('Results')
+            response = response_pe
             if response_pe['Response'][0] == 200 and response_pe['Results']['Result']:
                 get_pa = {'function': 'Select','table_name': 'paciente', 'where': [{'name': 'ID_Pessoa', 'operator': '=', 'value': response_pe['Results']['Result'][0]['ID']}]}
                 response_pa = self.response_in_DB(get_pa)
-                if response_pa['Response'][0] == 200 and response_pa['Results']['Result']:
-                    response = response_pa
+                response = response_pa
+                if len(response_pe['Results']['Result']):
                     for key in response_pe['Results']['Result'][0].keys():
                         response['Results']['Result'][0][key] = response_pe['Results']['Result'][0][key]
-            print('Paciente -> ', response)
-
+        return response
+    
     def get_pessoa(self, cpf:str):
         cpf = re.sub('[^0-9]+', '', cpf)
         get_pe = {'function': 'Select','table_name': 'pessoa', 'where': [{'name': 'CPF', 'operator': '=', 'value': cpf}]}
         response = self.response_in_DB(get_pe)
         return response
+    
     def cadastro_pa(self, value:dict):
-        pass
+        response = {'Response': (406, 'Failed')}
+        if len(value['CPF']) == 11:
+            response_pe = self.cadastro_pe(value)
+            response = response_pe
+            if response_pe['Results']['Response'][0] == 200:
+                id_pessoa = self.get_pessoa(value['CPF'])['Results']['Result'][0]['ID']
+                set_pa = {'function': 'Insert', 'table_name': 'paciente', 'values': [{'name': 'ID_Pessoa', 'value': id_pessoa}]}
+                response_pa = self.response_in_DB(set_pa)
+                response = response_pa
+        return response
     def cadastro_pe(self, value:dict):
         set_pe = {'function': 'Insert', 'table_name': 'pessoa', 'values': [{'name':'CPF', 'value': re.sub('[^0-9]+', '',value['CPF'])}, {'name':'Nome', 'value':re.sub('[^a-zA-Z ]+', '',value['Nome'])}, {'name':'Telefone', 'value': re.sub('[^0-9]+', '',value['Telefone'])}, {'name':'Email', 'value': value['Email']}, {'name':'CEP', 'value': re.sub('[^0-9]+', '',value['CEP'])}, {'name': 'Genero', 'value': value['Genero']}, {'name':'Nascimento', 'value': str(value['Nascimento'])}, {'name': 'Complem_Endereco', 'value': value['Complem_Endereco']}, {'name': 'Idade', 'value': int(value['Idade'])}]}
-        response_pa = self.response_in_DB(set_pe)
-        print(response_pa)
+        response_pe = self.response_in_DB(set_pe)
+        return response_pe
     def response_in_DB(self, get:dict):
         data = json.dumps(get, indent=2).encode('utf-8')
         response = {}
@@ -64,4 +73,7 @@ if __name__ == '__main__':
     p.get_paciente('108.543.894-58')
     
     print('___________________________')
-    p.cadastro_pe({'CPF': '', 'Nome': 'Francisco', 'Telefone': '81999932153', 'Email': 'luis.sda.3dqda@gmail.com', 'CEP': '51394123', 'Complem_Endereco': 'dasdadaNADa','Idade': 0, 'Genero': 'H', 'Nascimento': '15-03-2001'})
+    a = p.cadastro_pa({'CPF': '12223229227', 'Nome': 'Francisco', 'Telefone': '81999932153', 'Email': 'luis.sda.3dqda@gmail.com', 'CEP': '51394123', 'Complem_Endereco': 'dasdadaNADa','Idade': 0, 'Genero': 'H', 'Nascimento': '15-03-2001'})
+    print(a)
+    b = p.get_paciente('22223229227')
+    print(b)
